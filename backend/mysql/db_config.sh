7d4643e21da8a04ab4f573bb867c20690c40f855
Vitalii Nesterenko <nesvits@gmail.com>
2016-08-21 16:18:52 +0300

    db_config.sh creates database and adds permisiion for wordpress

--- backend/mysql/db_config.sh
+++ backend/mysql/db_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
 # create database for wordpress
 mysql -uroot -proot -e "CREATE DATABASE wordpress;"
 # create specific user in mysql
-mysql -uroot -proot -e "CREATE USER wordpressuser@localhost IDENTIFIED BY 'wordpressuser';"
+mysql -uroot -proot -e "CREATE USER wordpressuser@'10.10.100.20' IDENTIFIED BY 'password';"
 # grant permissions to all tables in database wordpress to user wordpressuser
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost;"
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@10.10.100.20;"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@'10.10.100.20' IDENTIFIED BY 'password';"
 # update permissions
 mysql -uroot -proot -e "FLUSH PRIVILEGES;"
